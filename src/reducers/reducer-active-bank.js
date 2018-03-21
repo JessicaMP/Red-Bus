f9161bf592931ac2b64c6d2006d22f077b904ab7
JessicaMP <jessica95.mp@gmail.com>
2018-03-21 16:11:45 -0500

    Corrigiendo reducer

--- src/reducers/reducer-active-bank.js
+++ src/reducers/reducer-active-bank.js
@@ -1,18 +1,8 @@
-const data = [ {name: 'bcp', id : 1, img:'../assets/images/bcp.png',  paso1: 'Selecciona la opción Pago de Servicios > EMPRESAS > PAGOEFECTIVO > PAGOEFECTIVOSOLES'} ,
-    {name: 'bbva', img:'../assets/images/bbva.png',  paso1: 'Selecciona la opción Pago de Servicios > De Instituciones y Empresas > Busca por nombre > PAGOEFECTIVO > PAGOEFECTIVOSOLES'}, 
-    {name: 'interbank', img:'../assets/images/interbank.png',  paso1: 'Selecciona la opción Pago a Instituciones o Empresa > Empresa PAGOEFECTIVO > Servicio PAGO EFECTIVO'}, 
-    {name: 'scotiabank', img:'../assets/images/scotiabank.png',  paso1: 'Selecciona la opción Pagos > Otras Instituciones > Otros > Busca por Empresa/Servicio: PAGOEFECTIVO > Selecciona: PAGOEFECTIVO SOLES'},
-    {name: 'banbif', img:'../assets/images/banbif.png',  paso1: 'Selecciona la opción Pago de Servicios > Busca por Empresa y escribe PAGO EFECTIVO > Seleciona la empresa PAGO EFECTIVO'}
-   ];
+export default function (state = {name: 'banbif', img:'banbif.png',  paso1: 'Selecciona la opción Pago de Servicios > Busca por Empresa y escribe PAGO EFECTIVO > Seleciona la empresa PAGO EFECTIVO'} , action){
+  switch (action.type) {
+    case 'BANK_SELECTED':
+    return action.payload;
+  }
+  return state;
 
-const info = {name: 'bcp', img:'bcp.png',  paso1: 'Selecciona la opción Pago de Servicios > EMPRESAS > PAGOEFECTIVO > PAGOEFECTIVOSOLES'} ;
-const bancos = (state = data, action) => {
-    switch (action.type) {
-      case 'BANK_SELECTED':    
-      return action.payload;
-    }
-    return state;
-  
 }
-
-export default bancos;
